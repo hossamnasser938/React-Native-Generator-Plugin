e4bde8a696b50d8bc6b7ce573aa321df25a9c597
Hossam <hossamnasser938@gmail.com>
2020-02-18 20:18:10 +0200

    plugin initial high level design

--- main.js
+++ main.js
@@ -1,23 +1,99 @@
-/*
- * Sample plugin scaffolding for Adobe XD.
- *
- * Visit http://adobexdplatform.com/ for API docs and more sample code.
- */
+const { Artboard } = require("scenegraph");
 
-const { Rectangle, Color } = require("scenegraph");
+let code = "";
 
-function rectangleHandlerFunction(selection) {
-  const newElement = new Rectangle();
-  newElement.width = 100;
-  newElement.height = 50;
-  newElement.fill = new Color("Purple");
+async function generate(selection, documentRoot) {
+  documentRoot.children.forEach(documentItem => {
+    if (documentItem instanceof Artboard) {
+      code += `{/* <Screen ${documentItem.name}> */}\n`;
+      loopOverTree(documentItem);
+      code += `{/* </Screen ${documentItem.name}> */}\n\n`;
+    } else {
+      console.log("not artboard, maybe hint", documentItem);
+    }
+  });
 
-  selection.insertionParent.addChild(newElement);
-  newElement.moveInParentCoordinates(100, 100);
+  console.log(code);
+}
+
+function loopOverTree(tree) {
+  if (tree.children && tree.children.length) {
+    code += `<View>\n`;
+
+    tree.children.forEach(innerTree => {
+      loopOverTree(innerTree);
+    });
+
+    code += `</View>\n`;
+  } else {
+    generateLeafNodeCode(tree);
+  }
+}
+
+function generateLeafNodeCode(leafNode) {
+  switch (leafNode.constructor.name) {
+    case "Rectangle":
+      code += generateRectangleCode(leafNode);
+      break;
+
+    case "Ellipse":
+      code += generateEllipseCode(leafNode);
+      break;
+
+    case "Polygon":
+      code += generatePolygonCode(leafNode);
+      break;
+
+    case "Line":
+      code += generateLineCode(leafNode);
+      break;
+
+    case "Path":
+      code += generatePathCode(leafNode);
+      break;
+
+    case "BooleanGroup":
+      code += generateBooleanGroupCode(leafNode);
+      break;
+
+    case "Text":
+      code += generateTextCode(leafNode);
+      break;
+
+    default:
+  }
+}
+
+function generateRectangleCode(rectangle) {
+  return `<Rectangle />\n`;
+}
+
+function generateEllipseCode(ellipse) {
+  return `<Ellipse />\n`;
+}
+
+function generatePolygonCode(polygon) {
+  return `<Polygon />\n`;
+}
+
+function generateLineCode(line) {
+  return `<Line />\n`;
+}
+
+function generatePathCode(path) {
+  return `<Path />\n`;
+}
+
+function generateBooleanGroupCode(booleanGroup) {
+  return `<Boolean />\n`;
+}
+
+function generateTextCode(text) {
+  return `<Text />\n`;
 }
 
 module.exports = {
   commands: {
-    createRectangle: rectangleHandlerFunction
+    generate
   }
 };
