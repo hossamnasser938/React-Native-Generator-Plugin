850f7ff2276c9bd93283b210259d3eeeea1d928b
Hossam <hossamnasser938@gmail.com>
2020-02-24 21:51:33 +0200

    test an idea for aligning widgets[yet to be extended]

--- generators/generateContainerCode.js
+++ generators/generateContainerCode.js
@@ -10,17 +10,55 @@ function generateContainerCode(container) {
   const children = getNodeChildren(container);
   let childrenCode = "";
 
-  children.forEach(child => {
-    childrenCode += generateLeafNodeCode(child);
-  });
-
   let style = {};
 
-  if (
-    children[1].globalBounds.y <
-    children[0].globalBounds.y + children[0].globalBounds.height
-  ) {
-    style.flexDirection = "row";
+  const n = children.length;
+
+  if (n === 1) {
+    childrenCode = generateLeafNodeCode(children[0]);
+  } else {
+    let childrenMatrix = new Array(n).fill(0).map(_ => new Array(n).fill(0));
+
+    childrenMatrix[0][0] = children[0];
+
+    if (
+      Math.abs(children[1].globalBounds.x - children[0].globalBounds.x) <
+      Math.abs(children[1].globalBounds.y - children[0].globalBounds.y)
+    ) {
+      // close X values -> more likely exist in one column
+      childrenMatrix[1][0] = children[1];
+
+      // assure that each one exists in its correct place relative to the other
+      if (
+        childrenMatrix[0][0].globalBounds.y >
+        childrenMatrix[1][0].globalBounds.y
+      ) {
+        const temp = childrenMatrix[0][0];
+        childrenMatrix[0][0] = childrenMatrix[1][0];
+        childrenMatrix[1][0] = temp;
+      }
+
+      childrenCode += generateLeafNodeCode(childrenMatrix[0][0]);
+      childrenCode += generateLeafNodeCode(childrenMatrix[1][0]);
+    } else {
+      // close Y values -> more likely exist in one row
+      childrenMatrix[0][1] = children[1];
+
+      // assure that each one exists in its correct place relative to the other
+      if (
+        childrenMatrix[0][0].globalBounds.x >
+        childrenMatrix[0][1].globalBounds.x
+      ) {
+        const temp = childrenMatrix[0][0];
+        childrenMatrix[0][0] = childrenMatrix[0][1];
+        childrenMatrix[0][1] = temp;
+      }
+
+      childrenCode += generateLeafNodeCode(childrenMatrix[0][0]);
+      childrenCode += generateLeafNodeCode(childrenMatrix[0][1]);
+
+      style.flexDirection = "row";
+    }
   }
 
   let code = `<View style={${JSON.stringify(style)}}>
