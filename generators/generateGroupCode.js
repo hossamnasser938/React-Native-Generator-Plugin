1b4dfc0e2e4f01fd9bb649bcbb5131cb8397289c
Hossam <hossamnasser938@gmail.com>
2020-02-29 22:04:10 +0200

    Build Group generator

--- generators/generateGroupCode.js
+++ generators/generateGroupCode.js
@@ -1,5 +1,12 @@
+const { generateContainerCode } = require("./generateContainerCode");
+const { getNodeChildren } = require("../helpers/getNodeChildren/index");
+
 function generateGroupCode(group) {
-  return `<Group />\n`;
+  const groupChildren = group.children.map(item => item);
+
+  const nodesInBounds = getNodeChildren(group);
+
+  return generateContainerCode([...groupChildren, ...nodesInBounds]);
 }
 
 module.exports = {
