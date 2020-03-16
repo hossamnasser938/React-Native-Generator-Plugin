f5d12706e50da85d469a9fa0182728c02c4a1df2
Hossam <hossamnasser938@gmail.com>
2020-03-16 21:14:28 +0200

    add a hint to K=LinkedGraphic generator

--- generators/generateLinkedGraphicCode.js
+++ generators/generateLinkedGraphicCode.js
@@ -1,5 +1,5 @@
 function generateLinkedGraphicCode(linkedGraphic) {
-  return `<LinkedGraphic />\n`;
+  return `{/* <LinkedGraphic /> {this is a node from an external resource like a Creative Cloud Library} */}\n`;
 }
 
 module.exports = {
