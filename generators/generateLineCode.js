8fc64bed0e2c917f9217752c6f6b0fb8df1e3b17
Hossam <hossamnasser938@gmail.com>
2020-03-21 19:02:39 +0200

    Use globalBounds

--- generators/generateLineCode.js
+++ generators/generateLineCode.js
@@ -12,25 +12,18 @@ function generateLineCode(line) {
 
   const style = {};
 
-  const {
-    start,
-    end,
-    strokeEnabled,
-    stroke,
-    strokeWidth,
-    boundsInParent
-  } = line;
+  const { start, end, strokeEnabled, stroke, strokeWidth, globalBounds } = line;
 
   if (strokeEnabled) {
     style.backgroundColor = stroke.toHex(true);
 
     if (start.x !== end.x) {
       // horizontal
-      style.width = pixelUnitPreprocessor(boundsInParent.width);
+      style.width = pixelUnitPreprocessor(globalBounds.width);
       style.height = pixelUnitPreprocessor(strokeWidth);
     } else {
       // vertical
-      style.height = pixelUnitPreprocessor(boundsInParent.height);
+      style.height = pixelUnitPreprocessor(globalBounds.height);
       style.width = pixelUnitPreprocessor(strokeWidth);
     }
   }
