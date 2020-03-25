e7b2c7e7cd67bff7d13f76c40399d33f6c2134a8
Hossam <hossamnasser938@gmail.com>
2020-03-25 21:45:45 +0200

    remove unnecessary spaces

--- src/helpers/output/prettifyCode.js
+++ src/helpers/output/prettifyCode.js
@@ -1,12 +1,14 @@
 const beautify = require("js-beautify");
 
 function prettifyCode(code) {
+  // prettify code using js-beatify
   const beautifiedComponent = beautify(code, {
     indent_size: 2,
     operator_position: "preserve-newline"
   });
 
-  return beautifiedComponent;
+  // remove white space added after opening tag
+  return beautifiedComponent.replace(new RegExp(/<\s/, "g"), "<");
 }
 
 module.exports = {
